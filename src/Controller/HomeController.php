e929284c0f0af5817874e65397adc7be3fe8e966
PELLETIER Samuel <pellesam.info@gmail.com>
2021-03-04 17:52:30 +0100

    New page home

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Controller;
+
+use App\Entity\Comment;
+use App\Entity\Post;
+use App\Event\CommentCreatedEvent;
+use App\Form\CommentType;
+use App\Repository\PostRepository;
+use App\Repository\TagRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Controller used to manage home contents in the public part of the site.
+ *
+ * @Route("/home")
+ *
+ */
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", methods="GET", name="home_index")
+     *
+     * NOTE: For standard formats, Symfony will also automatically choose the best
+     * Content-Type header for the response.
+     * See https://symfony.com/doc/current/routing.html#special-parameters
+     */
+    public function index(Request $request): Response
+    {
+        return $this->render('home/index.html.twig',);
+    }
+}
